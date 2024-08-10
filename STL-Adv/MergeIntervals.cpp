c0f7252b006aa46f388ed9dfd2ac782084ac7d88
Santrupti <behera.santrupti02@gmail.com>
2024-08-10 12:13:57 +0530

    Update MergeIntervals.cpp

--- STL-Adv/MergeIntervals.cpp
+++ STL-Adv/MergeIntervals.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> res;
+        res.push_back({arr[0][0], arr[0][1]});
+        for(int i=1; i<n; i++){
+            int j=res.size()-1;
+            if(arr[i][0]<=res[j][1])
+            {
+                if(arr[i][1]>res[j][1])
+                    res[j]={res[j][0], arr[i][1]};
+            }
+            else
+            {
+                res.push_back({arr[i][0], arr[i][1]});
+            }
+        }
+        return res;
+    }
+};
