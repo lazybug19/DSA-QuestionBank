97d0d887e7b2c6fd74bc1fc8d7b728ec2c40852d
Santrupti <behera.santrupti02@gmail.com>
2024-08-13 12:31:30 +0530

    Create LRUCache.cpp

---
+++ STL-Adv/LRUCache.cpp
@@ -0,0 +1,101 @@
+// Accessing already existing page in cache via queue is difficult so queue is implement using DLL
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+class LRUCache {
+  public:
+    class node {
+      public:
+        int key;
+      int val;
+      node * next;
+      node * prev;
+      node(int _key, int _val) {
+        key = _key;
+        val = _val;
+      }
+    };
+
+  node * head = new node(-1, -1);
+  node * tail = new node(-1, -1);
+
+  int cap;
+  unordered_map < int, node * > m;
+
+  LRUCache(int capacity) {
+    cap = capacity;
+    head -> next = tail;
+    tail -> prev = head;
+  }
+
+  void addnode(node * newnode) {
+    node * temp = head -> next;
+    newnode -> next = temp;
+    newnode -> prev = head;
+    head -> next = newnode;
+    temp -> prev = newnode;
+  }
+
+  void deletenode(node * delnode) {
+    node * delprev = delnode -> prev;
+    node * delnext = delnode -> next;
+    delprev -> next = delnext;
+    delnext -> prev = delprev;
+  }
+
+  int get(int key_) {
+    if (m.find(key_) != m.end()) {
+      node * resnode = m[key_];
+      int res = resnode -> val;
+      m.erase(key_);
+      deletenode(resnode);
+      addnode(resnode);
+      m[key_] = head -> next;
+      return res;
+    }
+
+    return -1;
+  }
+
+  void put(int key_, int value) {
+    if (m.find(key_) != m.end()) {      
+    node * existingnode = m[key_];
+      m.erase(key_);
+      deletenode(existingnode);
+    }
+    if (m.size() == cap) {
+      m.erase(tail -> prev -> key);
+      deletenode(tail -> prev);
+    }
+
+    addnode(new node(key_, value));
+    m[key_] = head -> next;
+  }
+};
+
+// Counting page faults in paging
+class Solution{
+public:
+    int pageFaults(int N, int C, int pages[]){
+        vector<int> v;
+        int faults=0;
+        for(int i=0; i<N; i++)
+        {
+            if(find(v.begin(), v.end(), pages[i])==v.end())
+            {
+                if(v.size()==C)
+                    v.erase(v.begin());
+                faults++;
+            }
+            else
+                v.erase(find(v.begin(), v.end(), pages[i]));
+            v.push_back(pages[i]);
+        }
+        return faults;
+    }
+};
+ 
