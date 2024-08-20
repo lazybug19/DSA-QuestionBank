5c2d214438982566d170baa233e3553e877fc424
Santrupti <behera.santrupti02@gmail.com>
2024-08-20 19:14:00 +0530

    Create Islands2_DSU.cpp

---
+++ Graphs/Islands2_DSU.cpp
@@ -0,0 +1,74 @@
+class Solution {
+  public: 
+  
+  int findPar(int node, vector<int>& par)
+  {
+    if(par[node]==node) return node;
+    return par[node]=findPar(par[node], par);
+  }
+  
+  void unionBySize(int u, int v, vector<int>& par, vector<int>& size)
+  {
+    int pu=findPar(u, par);
+    int pv=findPar(v, par);
+    if(pu==pv) return;
+    if(size[pu]<size[pv])
+    {
+        par[pu]=pv;
+        size[pv]+=size[pu];
+    }
+    else
+    {
+        par[pv]=pu;
+        size[pu]+=size[pv];
+    }
+  }
+  
+  bool isValid(int x, int y, int n, int m)
+  {
+    return x>=0 && x<n && y>=0 && y<m;
+  }
+  
+  vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators)
+  {
+    vector<vector<int>> matrix(n, vector<int>(m, 0));
+    int r[4] = {-1, 0, 0, 1};
+    int c[4] = {0, -1, 1, 0};
+    vector<int> res;
+    int cnt=0;
+    int k=operators.size();
+    for(int i=0; i<k; i++)
+    {
+        int row=operators[i][0];
+        int col=operators[i][1];
+        if(matrix[row][col]==1)
+        {
+            res.push_back(cnt);
+            continue;
+        }
+        else
+        {
+            matrix[row][col]==1;
+            cnt++;
+            
+            for(int j=0; j<4; j++)
+            {
+                int ar=row+r[j];
+                int ac=col+c[j];
+                if(isValid(ar, ac, n, m) && vis[ar][ac]==1)
+                {
+                    int ccell=row*m + col;
+                    int acell=ar*m + ac;
+                    if(findPar(ccell)!=findPar(acell)) // they belong to different clusters or sets
+                    {
+                        cnt--;
+                        unionBySize(ccell, acell, par, size);
+                    }
+                }
+                res.push_back(cnt);
+            }
+        }
+    }
+    return res;
+  }
+};
