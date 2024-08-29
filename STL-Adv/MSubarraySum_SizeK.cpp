25b2d0ee94cbe09538a3c1fbddc018eb7ff421a3
Santrupti <behera.santrupti02@gmail.com>
2024-08-29 17:45:02 +0530

    Create MSubarraySum_SizeK.cpp

---
+++ STL-Adv/MSubarraySum_SizeK.cpp
@@ -0,0 +1,6 @@
+/*
+Given an array and two numbers M and K. We need to find the max sum of sums of M subarrays of size K (non-overlapping) 
+in the array. (Order of array remains unchanged). K is the size of subarrays and M is the count of subarray. It may be 
+assumed that size of array is more than m*k. 
+*/
+
