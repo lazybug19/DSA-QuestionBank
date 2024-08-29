a04c608dd564315311aa761def9c8cf527e901b4
Santrupti <behera.santrupti02@gmail.com>
2024-08-29 15:57:40 +0530

    Create MaxSlidingWindow.cpp

---
+++ STL-Adv/MaxSlidingWindow.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int> & nums, int k) {
+    deque<int> dq;
+    vector<int> ans;
+    for(int i=0; i<nums.size(); i++) 
+    {
+        // equivalent to sliding k-sized window forward by removing first stored index
+        if(!dq.empty() && dq.front()==i-k) 
+            dq.pop_front(); 
+
+        // implementation of monotonic queue
+        while(!dq.empty() && nums[dq.back()]<nums[i])
+            dq.pop_back();
+        dq.push_back(i);
+
+        if(i>=k-1) ans.push_back(nums[dq.front()]);
+    }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
