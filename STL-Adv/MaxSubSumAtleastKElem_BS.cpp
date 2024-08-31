0b5aed613ceee70976611d15d6a3f0a8663fdc06
Santrupti <behera.santrupti02@gmail.com>
2024-08-31 18:11:33 +0530

    Update MaxSubSumAtleastKElem_BS.cpp

--- STL-Adv/MaxSubSumAtleastKElem_BS.cpp
+++ STL-Adv/MaxSubSumAtleastKElem_BS.cpp
@@ -3,3 +3,19 @@
   It is guaranteed that the size of array is at-least k.
 */
 
+class Solution{
+    public:    
+    long long int maxSumWithK(long long int a[], long long int n, long long int k) 
+    {
+        long long sumTake, sumSlide=0, mx;
+        for(int i=0; i<k; i++) sumSlide+=a[i];
+        sumTake = mx = sumSlide;
+        for(int i=k; i<n; i++)
+        {
+            sumSlide+=(a[i]-a[i-k]);
+            sumTake=max(sumTake+a[i], sumSlide);
+            mx=max(mx, sumTake);
+        }
+        return mx;
+    }
+};
