43a68192d85ac6a3f780000ef95a063f0ec52b97
Santrupti <behera.santrupti02@gmail.com>
2024-08-31 17:57:02 +0530

    Create MaxSubSumAtleastKElem_BS.cpp

---
+++ STL-Adv/MaxSubSumAtleastKElem_BS.cpp
@@ -0,0 +1,5 @@
+/*
+  Given an array a of length n and a number k, find the largest sum of the subarray containing at least k numbers. 
+  It is guaranteed that the size of array is at-least k.
+*/
+
