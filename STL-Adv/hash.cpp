3c89e2cb819a897a0f036e6da486c68c56a50151
lazybug <behera.santrupti02@gmail.com>
2024-09-27 19:17:18 +0530

    added: SW & BS

--- STL-Adv/hash.cpp
+++
@@ -1,1 +0,0 @@
-HASHING: Given an array with N distinct elements, convert the given array to a form where all elements are in the range from 0 to N-1
---
+++ SW/2-pointer/fruit_baskets.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v={2, 2, 1, 3, 4, 5, 5, 5, 4};
+    int k=3, n=9, start=0, i=0, len, mx=INT_MIN; //k=no. of baskets, n=no. of trees
+    unordered_map<int,int> mp;
+    while(i<n)
+    {
+        mp[v[i]]++;
+        while(mp.size()>k)
+        {
+            mp[v[start]]--;
+            if(mp[v[start]] == 0)
+                mp.erase(v[start]);
+            start++;
+        }
+        len=i-start+1;
+        mx=max(mx,len);
+        i++;
+    }
+    cout << mx;
+    return 0;
+}
